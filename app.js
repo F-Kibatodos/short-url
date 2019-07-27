5d8217fdbb7589e6ea9d58a48b4648168c09f74a
Brian.L <brian9500885@gmail.com>
2019-07-27 18:05:28 +0800

    initial project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000)
